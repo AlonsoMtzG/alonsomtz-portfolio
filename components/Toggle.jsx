d31b7d3185c84bea7f6983e24da24bbbaf23c741
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-12 20:09:45 -0600

    minor fix

--- components/Toggle.jsx
+++ components/Toggle.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export const Toggle = ({ label }) => {
   return (
-    <label class="relative inline-flex cursor-pointer">
-      <input type="checkbox" value="" class="sr-only peer" />
-      <div class="w-9 h-5 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-      <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+    <label className="relative inline-flex cursor-pointer">
+      <input type="checkbox" value="" className="sr-only peer" />
+      <div className="w-9 h-5 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
         {label}
       </span>
     </label>
