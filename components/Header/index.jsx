5c4b9b6d18b7d76176fb609769b4611ecea0a5bd
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-14 03:43:58 -0600

    update header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,11 +1,34 @@
+'use client';
 import React from 'react';
+import Typewriter from 'typewriter-effect';
+
+import { useThemeContext } from '@/hooks/useThemeContext';
 import { LangButton } from './LangButton';
 
 export const Header = () => {
+  const { textColorSelected } = useThemeContext();
+
   return (
-    <div className="flex px-10 py-10 items-center justify-between">
-      <div />
-      <span className="font-satoshi font-bold text-xl">Alonso Mtz</span>
+    <div className="flex px-10 py-14 items-center justify-between">
+      <span className="font-satoshi font-bold">Alonso Mtz</span>
+      <p className="font-light flex gap-1">
+        passionate about{' '}
+        <span
+          className={`font-semibold ${textColorSelected} transition-all duration-300`}
+        >
+          <Typewriter
+            options={{
+              strings: [
+                'crafting memorable user experiences.',
+                'merging code and creativity.',
+                'building digital ecosystems.',
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </span>
+      </p>
       <LangButton />
     </div>
   );
