4b5724de95030e0267352277d81a431ccf3f07ac
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-14 03:43:20 -0600

    deleted text from about

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -1,25 +1,11 @@
-'use client';
 import React from 'react';
 
-import { useThemeContext } from '@/hooks/useThemeContext';
 import { TextMarquee } from './TextMarquee';
 
 export const About = () => {
-  const { textColorSelected } = useThemeContext();
-
   return (
     <div className="py-10">
       <TextMarquee />
-      <div className="px-10">
-        <p className="font-light">
-          Hi I'm Alonso Martínez, passionate about crafting{' '}
-          <span
-            className={`font-semibold ${textColorSelected} transition-all duration-300`}
-          >
-            memorable user experiences.
-          </span>
-        </p>
-      </div>
     </div>
   );
 };
