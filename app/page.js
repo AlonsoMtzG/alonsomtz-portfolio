6080d4d9b6bd0f143f055ff53e1eb5292448acc4
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-18 18:06:25 -0600

    show content

--- app/page.js
+++ app/page.js
@@ -1,19 +1,17 @@
-import { Building } from "@/components/Building";
+import { About } from "@/components/About";
+import { Divider } from "@/components/Divider";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { Projects } from "@/components/Projects";
+// import { Building } from "@/components/Building";
 
 export default function Home() {
   return (
     <main className="font-satoshi">
-      {/* <Header />
-      <Divider />
+      <Header />
       <About />
-      <Divider />
       <Projects />
-      <Divider />
-      <Footer /> */}
-      {/* Delete when ending build  */}
-      <div className="h-[100dvh] flex justify-center items-center bg-[url('/1.png')] bg-cover">
-        <Building />
-      </div>
+      <Footer />
     </main>
   );
 }
