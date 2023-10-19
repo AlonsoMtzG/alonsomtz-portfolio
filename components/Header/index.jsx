eca868c4a72e548c48bc1e9a8cca91cc3d1661c6
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-18 18:23:18 -0600

    update header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,34 +1,13 @@
 'use client';
 import React from 'react';
-import Typewriter from 'typewriter-effect';
 
-import { useThemeContext } from '@/hooks/useThemeContext';
 import { LangButton } from './LangButton';
+import Image from 'next/image';
 
 export const Header = () => {
-  const { textColorSelected } = useThemeContext();
-
   return (
-    <div className="flex px-10 py-14 items-center justify-between">
-      <span className="font-satoshi font-bold">Alonso Mtz</span>
-      <p className="font-light flex gap-1">
-        passionate about{' '}
-        <span
-          className={`font-semibold ${textColorSelected} transition-all duration-300`}
-        >
-          <Typewriter
-            options={{
-              strings: [
-                'crafting memorable user experiences.',
-                'merging code and creativity.',
-                'building digital ecosystems.',
-              ],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </span>
-      </p>
+    <div className="flex py-14 items-center justify-between">
+      <Image src="/am-logo.svg" width={50} height={50} priority />
       <LangButton />
     </div>
   );
