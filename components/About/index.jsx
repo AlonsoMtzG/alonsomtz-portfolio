6c4a578cadf33374b753092ebfdbd1ade18aaed1
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-18 18:23:23 -0600

    update about

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -1,11 +1,30 @@
+'use client';
 import React from 'react';
-
-import { TextMarquee } from './TextMarquee';
+import Typewriter from 'typewriter-effect';
 
 export const About = () => {
   return (
     <div className="py-14">
-      <TextMarquee />
+      <h1 className="font-medium text-4xl">Software Engineer</h1>
+      <h3 className="font-thin text-zinc-400 text-lg">
+        Full Stack Development | UI/UX Design
+      </h3>
+      <div className="font-thin flex gap-1 text-zinc-400 text-lg">
+        passionate about{' '}
+        <span className="font-medium transition-all duration-300">
+          <Typewriter
+            options={{
+              strings: [
+                'crafting memorable user experiences.',
+                'merging code and creativity.',
+                'building digital ecosystems.',
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </span>
+      </div>
     </div>
   );
 };
