ddd4a372e6689c9f8b64a3507c0b523b030c3dac
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-20 11:21:23 -0600

    update projects

--- components/Projects/index.jsx
+++ components/Projects/index.jsx
@@ -11,42 +11,42 @@ export const Projects = ({ divRef }) => {
   const { selectedProject } = useThemeContext();
 
   return (
-    <section className="flex">
-      <div className="flex flex-1 mr-10">
-        <Image
-          priority={true}
-          width={1920}
-          height={1440}
-          className="w-full"
-          src={projects[selectedProject].src}
-          alt={projects[selectedProject].name}
-        />
-      </div>
-      <div className="flex flex-1 flex-col gap-8">
-        <div className="flex flex-col">
-          <span className="text-right text-6xl font-medium text-indigo-600">
-            01
-          </span>
-          <span ref={divRef} className="text-right text-2xl font-medium">
-            Pixelu Studio
-          </span>
-        </div>
-        <div>
-          <div className="flex items-center gap-4 justify-end">
-            {projects[selectedProject].techs.map((val, idx) => (
-              <div key={val + idx} className="flex items-center">
-                <span className="text-zinc-400 text-xl font-thin mr-2">
-                  {val}
-                </span>
-                {idx < projects[selectedProject].techs.length - 1 && (
-                  <TechsCircle />
-                )}
-              </div>
-            ))}
+    <section className="flex justify-center">
+      <div className="flex-col items-center w-1/2">
+        <div className="flex flex-1 flex-col gap-8 mb-10">
+          <div className="flex flex-col text-center">
+            <span className="text-6xl font-medium text-indigo-600">01</span>
+            <span ref={divRef} className="text-2xl font-medium">
+              Pixelu Studio
+            </span>
           </div>
-          <p className="text-right text-xl font-thin">
-            {projects[selectedProject].description}
-          </p>
+          <div>
+            <div className="flex items-center gap-4 justify-center">
+              {projects[selectedProject].techs.map((val, idx) => (
+                <div key={val + idx} className="flex items-center">
+                  <span className="text-zinc-400 text-xl font-thin mr-2">
+                    {val}
+                  </span>
+                  {idx < projects[selectedProject].techs.length - 1 && (
+                    <TechsCircle />
+                  )}
+                </div>
+              ))}
+            </div>
+            <p className="text-xl font-thin text-center">
+              {projects[selectedProject].description}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-1">
+          <Image
+            priority={true}
+            width={1920}
+            height={1440}
+            className="w-full"
+            src={projects[selectedProject].src}
+            alt={projects[selectedProject].name}
+          />
         </div>
       </div>
     </section>
