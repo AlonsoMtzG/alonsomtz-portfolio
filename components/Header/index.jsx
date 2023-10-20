ee21d60043d2c144e7f4df3a62f65a268898101f
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-20 10:16:57 -0600

    removed lang button

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,14 +1,11 @@
 'use client';
 import React from 'react';
-
-import { LangButton } from './LangButton';
 import Image from 'next/image';
 
 export const Header = () => {
   return (
     <div className="flex py-10 items-center justify-between">
       <Image src="/am-logo.svg" width={50} height={50} priority />
-      <LangButton />
     </div>
   );
 };
