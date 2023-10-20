32301370e3c44b5da33958128cff6d7c55acc921
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-20 10:16:22 -0600

    removed floatingui

--- app/page.js
+++ app/page.js
@@ -1,18 +1,11 @@
-"use client";
-import { useState } from "react";
-
 import { Projects } from "@/components/Projects";
 import { About } from "@/components/About";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import { FloatingUI } from "@/components/FloatingUI";
 
 export default function Home() {
-  const [currentSection, setCurrentSection] = useState(0);
-
   return (
     <main className="font-satoshi px-14 h-screen relative">
-      <FloatingUI currentSection={currentSection} />
       <Header />
       <About />
       <Projects />
