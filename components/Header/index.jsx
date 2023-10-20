e2e39391a489dab912522bca4a61cc13c5f10d5e
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2023-10-20 16:23:22 -0600

    minor fix

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export const Header = () => {
   return (
     <header className="flex py-10 items-center justify-between">
-      <Image src="/am-logo.svg" width={50} height={50} priority />
+      <Image src="/am-logo.svg" width={50} height={50} priority alt="AM" />
     </header>
   );
 };
